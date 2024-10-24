abfe84a9d62ba08cde627e427339e2f5197fbb2e
Kevin-Sean-Chen <kschen@princeton.edu>
2024-10-24 19:17:30 -0400

    embedding and clustering for behavior

--- embed_test.py
+++ embed_test.py
@@ -137,10 +137,15 @@ feature_pos = build_features(vec_xy)
 ###############################################################################
 # %%
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import KMeans
 cmap = plt.get_cmap('tab10')
 
 agg_clust = AgglomerativeClustering(n_clusters=5)
 labels = agg_clust.fit_predict(feature_vxy)
+
+kmeans = KMeans(n_clusters=5)
+labels = kmeans.fit_predict(feature_vxy)
+centroids = kmeans.cluster_centers_
 
 # %%
 plt.figure()
@@ -158,6 +163,7 @@ for ii in range(5):
     
 # %% use U-map to visualize
 import umap
+from mpl_toolkits.mplot3d import Axes3D
 
 # Step 1: Generate clustered data (T x feature) and labels (T x 1)
 T = 500  # Number of data points
@@ -167,7 +173,7 @@ features = 10  # Number of features
 data, labels = feature_vxy, labels
 
 # Step 2: Apply UMAP to reduce dimensionality to 2D
-reducer = umap.UMAP(n_components=2, random_state=42)
+reducer = umap.UMAP(n_components=3, random_state=42)
 data_2d = reducer.fit_transform(data)
 
 # Step 3: Plot the 2D data and color-code according to the labels
@@ -175,23 +181,25 @@ unique_labels = np.unique(labels)
 # cmap = plt.get_cmap('tab10', len(unique_labels))
 
 # Step 4: Plot the 2D data and color-code according to the labels
-plt.figure(figsize=(10, 6))
-
+fig = plt.figure(figsize=(10, 6))
+ax = fig.add_subplot(111, projection='3d')
 # Use the `cmap` to color each class
 # scatter = plt.scatter(data_2d[:, 0], data_2d[:, 1], c=labels, cmap=cmap, s=50, alpha=0.8)
 
 for ii in range(5):
     pos = np.where(labels==ii)[0]
-    plt.scatter(data_2d[pos, 0], data_2d[pos, 1], cmap=cmap(ii), s=50, alpha=0.8)
+    # plt.scatter(data_2d[pos, 0], data_2d[pos, 1], cmap=cmap(ii), s=50, alpha=0.8)
+    ax.scatter(data_2d[pos, 0], data_2d[pos, 1], data_2d[pos, 2], cmap=cmap(ii), s=10, alpha=0.8)
 
 # Add a color bar for the classes
 # cbar = plt.colorbar(scatter, ticks=unique_labels)
 # cbar.set_label('Classes')
 
 # Add plot title and labels
-plt.title("2D UMAP projection of clustered tracks")
-plt.xlabel("UMAP Dimension 1")
-plt.ylabel("UMAP Dimension 2")
+ax.set_title("3D UMAP projection of clustered tracks")
+ax.set_xlabel("UMAP Dimension 1")
+ax.set_ylabel("UMAP Dimension 2")
+ax.set_zlabel("UMAP Dimension 2")
 plt.grid(True)
 
 # %% conditioning on odor on or off!
@@ -207,7 +215,7 @@ feat_ids = build_signal(vec_ids)
 
 # %% condition on odor
 odor_on = np.where(np.sum(feat_odor,1)>0)[0]
-odor_off = np.where((feat_time[:,0]>45+30) & (feat_time[:,-1]<45+30+10))[0]
+odor_off = np.where((feat_time[:,0]>45+30+1) & (feat_time[:,-1]<45+30+20))[0]
 
 plt.figure()
 plt.scatter(data_2d[:, 0], data_2d[:, 1], color='k', s=5, alpha=0.8)
@@ -259,13 +267,13 @@ plt.xlabel("X Axis")
 plt.ylabel("Y Axis")
 
 # %% projecting onto LDA
-proj_value = np.dot(data_2d[odor_on,:], lda_direction.T).reshape(-1)
+proj_value = np.dot(data_2d[odor_on,:]-center[None,:], lda_direction.T).reshape(-1)
 prev_odor = np.sum(feat_odor[odor_on],1)/window
 plt.figure()
-plt.plot(prev_odor, -proj_value,'b.')
+plt.plot(prev_odor, proj_value,'b.')
 plt.xlabel('experienced odor')
 plt.ylabel('proejction')
-# plt.ylim([-6,4])
+plt.ylim([-6,6])
 
 # %% projections conditioned on past odor!
 def last_argmin(arr):
@@ -285,15 +293,15 @@ for tt in range(len(odor_off)):
     if (vectorized_id[close_time_pos1] == vectorized_id[-1]) or (vectorized_id[close_time_pos2] == vectorized_id[-1]):  # of the same track
         vectorized_signal = feat_odor[:pos,:].reshape(-1)  ### might want to figure out when it ends
         proj_val_off.append(np.dot(data_2d[pos,:]-center, lda_direction.T))
-        prev_odor_off.append(np.sum(vectorized_signal[close_time_pos1:pos*window]) / len(np.where(vectorized_signal[close_time_pos1:pos*window]>0)[0]) )
+        prev_odor_off.append(np.mean(vectorized_signal[close_time_pos1:pos*window])/1)# / len(np.where(vectorized_signal[close_time_pos1:pos*window]>0)[0]) )
         # print(pos)
 
-# %%
+## %%
 plt.figure()
 plt.plot(prev_odor_off, proj_val_off, 'r.')
 plt.xlabel('experienced odor before off')
 plt.ylabel('proejction of off response')
-# plt.ylim([-6,4])
+plt.ylim([-6,6])
 
 # %% by fine time points...
 # # data4fit = []  # list of tracks with its vx,vy,theta signal recorded;  conditioned on behavior and long-tracks
@@ -323,3 +331,57 @@ plt.ylabel('proejction of off response')
 # plt.xlabel('experienced odor before off')
 # plt.ylabel('proejction of off response')
 # plt.ylim([-6,4])
+
+# %% idea: study state diversity
+def compute_dispersion(X_new, centroids):
+    # Assign each new data point to the nearest centroid
+    distances = np.linalg.norm(X_new[:, np.newaxis] - centroids, axis=2)
+    closest_centroids = np.argmin(distances, axis=1)
+    
+    # Calculate the dispersion (MSE) for each assigned cluster
+    dispersion = []
+    for i in range(len(centroids)):
+        # Get points assigned to the i-th centroid
+        cluster_points = X_new[closest_centroids == i]
+        
+        if cluster_points.size > 0:
+            # Calculate the distances from the points to the centroid
+            centroid = centroids[i]
+            # Calculate squared distances
+            squared_distances = np.linalg.norm(cluster_points - centroid, axis=1) ** 2
+            mse = np.mean(squared_distances)  # Mean Squared Error
+            dispersion.append(mse)
+        else:
+            dispersion.append(0)  # No points in this cluster
+
+    # Total dispersion
+    total_dispersion = np.sum(dispersion)
+    
+    return dispersion, total_dispersion
+
+from sklearn.metrics import silhouette_score
+time_window = np.arange(5,45+30+60, 4)
+reps = 10
+diversity = np.zeros((reps, len(time_window)))
+for rr in range(reps):
+    for tt in range(len(time_window)-1):
+        pos = np.where((feat_time[:,0]>time_window[tt]) & (feat_time[:,2]<time_window[tt+1]))[0]
+        sub_feat = feature_vxy[pos,:]
+        # agg_clustering_test = AgglomerativeClustering(n_clusters=5)
+        # agg_clustering_test.fit(sub_feat)
+        
+        kmeans = KMeans(n_clusters=5)
+        labels = kmeans.fit_predict(sub_feat)
+        centroids = kmeans.cluster_centers_
+        
+        dispersion, total_dispersion = compute_dispersion(sub_feat, centroids)
+        
+        # closest_centroids = kmeans.predict(sub_feat)
+        # dispersion = silhouette_score(sub_feat, closest_centroids)
+        diversity[rr,tt] = total_dispersion 
+    print(rr)
+# %%
+plt.figure()
+plt.plot(time_window[:-1], diversity[:,:-1].T, 'k-o',alpha=0.5)
+plt.axvline(x=45, color='r', linestyle='--'); plt.axvline(x=45+30, color='r', linestyle='--')
+plt.xlabel('time in trial (s)'); plt.ylabel('dispersion from centroids')
