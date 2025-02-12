1c259cf467b801d7637d380721de16d1f76afed9
Kevin-Sean-Chen <kschen@princeton.edu>
2025-02-12 18:15:36 -0500

    validate inference

--- hmm/driven_chain.py
+++ hmm/driven_chain.py
@@ -14,21 +14,28 @@ from scipy.sparse import diags
 from scipy.sparse.linalg import eigs
 from scipy.linalg import hankel
 import random
+from scipy.optimize import minimize
 
 import seaborn as sns
 sns.set_style("white")
 sns.set_context("talk")
 
 # %% model setup
-lt = 10000
+lt = 5000
 N = 5
-Pij_true = np.random.rand(N,N) + np.eye(N)*5   # baseline transition
+u_dim = 2
+Pij_true = np.random.rand(N,N) + np.eye(N)*1   # baseline transition
 Pij_true = Pij_true / Pij_true.sum(axis=1, keepdims=True)
-w_true = np.random.randn(N)*.1  # weights on the input  ##### explore NxN later???
+w_true = np.random.randn(N, u_dim)*2  # weights on the input  ##### explore NxN later???
 
-smooth_lt = 10
-stim = np.random.randn(lt)
+smooth_lt = 1
+stim = np.random.randn(lt)*1
 stim = np.convolve(stim, np.ones(smooth_lt), mode='same')  # smooth external input
+
+stim = np.random.randn(lt,u_dim)
+
+stim[stim>2] = 2
+stim[stim<2] = 0
 
 # %% functional
 def driven_markov_chain(P, weights, ipts, init=None):
@@ -52,7 +59,8 @@ def driven_markov_chain(P, weights, ipts, init=None):
     # Sample the next state based on the current state and transition matrix
     for t in range(1, n_steps):
         current_state = states[t - 1]
-        Pt = np.exp(weights[current_state]*ipts[t] + np.log(P[current_state]))
+        Pt = np.exp(np.dot(weights[current_state],ipts[t]) + np.log(P[current_state]))
+        # Pt = np.exp(weights[current_state]*ipts[t] + np.log(P[current_state]))  ### for scalar input
         Pt = np.round(Pt, decimals=6)
         Pt = Pt/np.sum(Pt)
         next_state = np.random.choice(n_states, p=Pt[:])
@@ -62,3 +70,89 @@ def driven_markov_chain(P, weights, ipts, init=None):
 states = driven_markov_chain(Pij_true, w_true, stim)
 
 # %% inferr weights and transition from data
+
+class log_linear_Markov:
+    def __init__(self, N, u_dim):
+        self.N = N
+        self.u_dim = u_dim
+        self.P00 = Pij_true #np.random.rand(N, N)  # ### 
+        self.P00 /= self.P00.sum(axis=1, keepdims=True)  ### baseline transition
+        self.logP00 = np.log(self.P00)
+        self.w0 = w_true #np.random.randn(N, u_dim)  #  #   ### stimulus weights
+        self.w_regu = 0
+    
+    def transition_prob(self, x, u):
+        """
+        Compute P(x' | x, u) using log-linear form.
+        """
+        correction = np.dot(self.ws, u) #self.ws*u  ### use scalar or vector form
+        P = np.exp(correction + self.logP0)
+        return P / P.sum(axis=1, keepdims=True)
+    
+    def log_likelihood(self, params, x_seq, u_seq):
+        self.logP0 = params[:self.N*self.N].reshape(self.N, self.N)
+        self.ws = params[self.N*self.N:].reshape(self.N, self.u_dim)
+        
+        ll = 0
+        for tt in range(len(x_seq)-1):
+            P = self.transition_prob(x_seq[tt], u_seq[tt])
+            tempP = P[ x_seq[tt], x_seq[tt+1] ]
+            # ll += tempP
+            if tempP>0:
+                ll += np.log(tempP)
+            # else:
+                # ll = -np.inf
+        return -ll + self.w_regu * np.sum(self.ws**2)
+    
+    def row_sum_constraint(self, params):
+        self.logP = params[:self.N*self.N].reshape(self.N, self.N)
+        cnt = np.exp(self.logP).sum(1)
+        return cnt - 1  # Each row sum should be 1
+
+    def fit(self, x_seq, u_seq):
+        """
+        Optimize parameters using MLE.
+        """
+        params = np.concatenate([self.logP00.flatten(), self.w0.flatten()])
+        constraints = {'type': 'eq', 'fun': self.row_sum_constraint}
+        # result = minimize(self.log_likelihood, params, args=(x_seq, u_seq), method='L-BFGS-B')
+        result = minimize(lambda pars: self.log_likelihood(pars, x_seq, u_seq), params, constraints=constraints)
+        print(result)
+        learned_param = result.x
+        self.P0 = np.exp(learned_param[:self.N*self.N].reshape(self.N, self.N))
+        self.ws = learned_param[self.N*self.N:].reshape(self.N, self.u_dim)
+
+# Example Usage
+# N = 5  # States
+model = log_linear_Markov(N, u_dim)
+model.w_regu = 0
+model.fit(states, stim)
+
+# %%
+plt.figure()
+plt.plot(Pij_true.flatten(), model.P0.flatten(),'.'); plt.xlabel('true P0'); plt.ylabel('inferred P0')
+plt.figure()
+plt.plot(w_true.flatten(), model.ws.flatten(),'.'); plt.xlabel('true w'); plt.ylabel('inferred w')
+
+# %% simulate
+def compute_markov_transition_matrix(states):
+    unique_states = np.unique(states)  # Identify unique states
+    num_states = len(unique_states)  # Number of states
+    state_map = {state: i for i, state in enumerate(unique_states)}
+    transition_counts = np.zeros((num_states, num_states))
+    for t in range(len(states) - 1):
+        i = state_map[states[t]]  # Current state index
+        j = state_map[states[t+1]]  # Next state index
+        transition_counts[i, j] += 1
+    # Normalize rows to get transition probabilities
+    P = transition_counts / transition_counts.sum(axis=1, keepdims=True)
+    P = np.nan_to_num(P)  # Handle divisions by zero
+    return P, state_map
+
+### simulations
+states_sim = driven_markov_chain(model.P0, model.ws, stim)
+Peff_true,_ = compute_markov_transition_matrix(states)
+Peff_sim,_ = compute_markov_transition_matrix(states_sim)
+plt.figure()
+plt.subplot(121); plt.imshow(Peff_true); plt.title('emperical')
+plt.subplot(122); plt.imshow(Peff_sim); plt.title('simulated')
