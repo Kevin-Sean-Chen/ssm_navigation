13dd0a2b592af63e38db1b38c7992ceaf8e44df2
Kevin-Sean-Chen <kschen@princeton.edu>
2025-02-20 20:36:21 -0500

    mode analyis of driven FHN

--- embed_FHN.py
+++ embed_FHN.py
@@ -22,16 +22,26 @@ a = 0.7
 b = 0.8
 epsilon = 0.08
 dt = 0.1  # Time step for Euler method
-T = 10000  # Total time
+T = 50000  # Total time
 num_steps = int(T / dt)  # Number of steps
 
 # Define input current I(t) (time-dependent input)
 def input_current(t):
-    return 0.1 * (np.sin(0.2 * t) + 1) + np.random.randn()*.1  # Example: oscillating input
+    current = .0 * (np.sin(0.9 * t) + 1) + np.random.randn()*.5
+    if t>10000:
+        current = 0. * (np.sin(0.2 * t) + 1) + np.random.randn()*.5
+        # current = 0.5
+        
+    # if current>0:
+    #     current=1
+    # else:
+    #     current=-1
+    return current  # Example: oscillating input
 
 # Initialize v and w
 v = np.zeros(num_steps)
 w = np.zeros(num_steps)
+iptt = np.zeros(num_steps)
 
 # Initial conditions (v0, w0)
 v[0] = -1.0  # Initial membrane potential
@@ -43,16 +53,25 @@ for t in range(1, num_steps):
     time = t * dt
     
     # FitzHugh-Nagumo equations
-    dvdt = v[t-1] - (v[t-1]**3) / 3 - w[t-1] + input_current(time)
+    iptt[t] = input_current(time)*1
+    dvdt = v[t-1] - (v[t-1]**3) / 3 - w[t-1] + iptt[t]
     dwdt = epsilon * (v[t-1] + a - b * w[t-1])
     
+    ### test bifurcation
+    if time>10000 and time<12000:
+        dvdt += 0.2
+    if time>20000 and time<22000:
+        dvdt += 0.2
+    if time>30000 and time<32000:
+        dvdt += 0.2
+        
     # Euler update for v and w
     v[t] = v[t-1] + dvdt * dt
     w[t] = w[t-1] + dwdt * dt
 
 # Plot the results
 time = np.linspace(0, T, num_steps)
-stim_vec = np.array([input_current(t) for t in time])
+stim_vec = iptt  #np.array([input_current(t) for t in time])
 
 plt.figure(figsize=(10, 6))
 
@@ -84,8 +103,8 @@ v_data = v*1
 
 # %% embedding Markov
 N_star = 1000
-K_star = 100
-tau_star = 5
+K_star = 150
+tau_star = 2
 
 # %% functional
 def build_signal(data, K=K_star):
@@ -97,7 +116,10 @@ def build_signal(data, K=K_star):
         features.append(vx)
     return np.vstack(features)
 
-X = build_signal(v_data)#, use_dtheta=True)
+X_volt = build_signal(v_data)#, use_dtheta=True)
+X_stim = build_signal(stim_vec)
+X = X_volt*1 #
+# X = np.concatenate((X_volt, X_stim), 1)
 
 # %% clustering and assigning states
 from sklearn.cluster import KMeans
@@ -187,7 +209,7 @@ plt.show()
 # %% plotting, compared to ground truth!
 import umap
 
-sub_samp = np.random.choice(X.shape[0], 20000, replace=False)
+sub_samp = np.random.choice(X.shape[0], 10000, replace=False)
 reducer = umap.UMAP(n_components=3, random_state=42)
 data_2d = reducer.fit_transform(X[sub_samp,:])
 
@@ -211,11 +233,21 @@ plt.ylabel('relaxation time (s)')
 plt.xlabel('eigenvalue index')
 
 # %% color code tracks]
-imode = 2
+imode = 1
 phi2 = eigvecs[labels,imode].real
-window_show = np.arange(1,10000)
+color_abs = np.max(np.abs(phi2))
+window_show = np.arange(50000,250000)
 plt.figure()
 plt.scatter(window_show, v_data[window_show],c=phi2[window_show],cmap='coolwarm',s=.1,vmin=-color_abs,vmax=color_abs)
+plt.title(f'mode#{imode}')
+
+# %% color code stims
+imode = 1
+phi2 = eigvecs[labels,imode].real
+color_abs = np.max(np.abs(phi2))
+window_show = np.arange(50000,150000)
+plt.figure()
+plt.scatter(window_show, stim_vec[window_show],c=phi2[window_show],cmap='coolwarm',s=.1,vmin=-color_abs,vmax=color_abs)
 plt.title(f'mode#{imode}')
 
 # %% now, analyze driving part!!!
@@ -225,4 +257,25 @@ plt.title(f'mode#{imode}')
 ### compute P(x'|x, u)
 ### infer weights on the input
 ### compute TE
-# %%
+# %% stim avarage
+###
+# deal with threshold crossing here!!!
+###
+# IDEA: it has to be delayed!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+###
+def find_threshold_crossings(time_series, threshold, direction="down"):
+    above_threshold = time_series > threshold
+    changes = np.diff(above_threshold.astype(int))  # Compute changes
+
+    if direction == "up":
+        crossings = np.where(changes == 1)[0]  # Crossing from below to above
+    elif direction == "down":
+        crossings = np.where(changes == -1)[0]  # Crossing from above to below
+    else:
+        raise ValueError("Direction must be 'up' or 'down'")
+    return crossings
+pos = np.where(phi2>-0.01)[0]
+# pos = find_threshold_crossings(phi2, -0.01,'down')
+plt.figure()
+plt.plot(np.mean(X_stim[pos-0,:],0)) ### <---- this result should depent on the FHN parameter!!! ex, oscillation vs. excited!
+plt.xlabel('time steps'); plt.ylabel('mode-triggered average'); #plt.ylim([-0.009,0.006])
